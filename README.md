75a9678a606210f7cfc8f5a883277e33b6562c87
Kush Surti <kushsurti05@gmail.com>
2022-11-26 12:17:46 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Counter Example
+This is a project assignment where Javascript was used to create simple counter with increment, decrement and clear functionalities.
---
+++ script.js
@@ -0,0 +1,34 @@
+var incrementButton = document.querySelector("#increment");
+var decrementButton = document.querySelector("#decrement");
+var clearButton = document.querySelector("#clear");
+var toggleButton = document.querySelector("#toggleTheme");
+
+var count = document.querySelector(".count");
+var error = document.querySelector(".error");
+
+incrementButton.addEventListener("click", changeCount);
+decrementButton.addEventListener("click", changeCount);
+clearButton.addEventListener("click", changeCount);
+function changeCount(event) {
+  error.hidden = true;
+
+  var action = event.target.innerText.toLowerCase();
+  var currentCount = Number(count.innerText);
+
+  if (action == "increment") {
+    count.innerText = currentCount + 1;
+  } else if (action == "decrement") {
+    if (currentCount > 0) {
+      count.innerText = currentCount - 1;
+    } else {
+      error.hidden = false;
+    }
+  } else if (action == "clear") {
+    count.innerText = 0;
+  }
+}
+toggleButton.addEventListener("click", changeTheme);
+function changeTheme(event) {
+  document.body.classList.toggle("bg-light");
+  document.body.classList.toggle("bg-dark");
+}
